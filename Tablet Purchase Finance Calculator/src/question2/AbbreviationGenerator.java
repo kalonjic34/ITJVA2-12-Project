ca99c265493dda656458eb99832cf1a54ece0556
Christian <kalonjic34@gmail.com>
2024-03-22 21:38:45 +0200

    created a jframe and jpanel to hold components

--- Tablet Purchase Finance Calculator/src/question2/AbbreviationGenerator.java
+++ Tablet Purchase Finance Calculator/src/question2/AbbreviationGenerator.java
@@ -6,5 +6,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AbbreviationGenerator {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Abbreviation Generator");
+        frame.setSize(400, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        placeComponents(panel);
+        // Make the frame visible
+        frame.setVisible(true);
+
+    }
 
 }
