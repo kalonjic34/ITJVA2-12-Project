1ba2c57b75b7af75f1679d5f05dbe118da864666
Christian <kalonjic34@gmail.com>
2024-03-22 21:36:55 +0200

    created a jframe and jpanel to hold components

---
+++ Tablet Purchase Finance Calculator/src/question2/AbbreviationGenerator.java
@@ -0,0 +1,10 @@
+package question2;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AbbreviationGenerator {
+
+}
