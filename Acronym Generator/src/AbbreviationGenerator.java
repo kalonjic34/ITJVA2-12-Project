5d5f9053428e1a08a53aeb881086dfb1a38a9882
Christian <kalonjic34@gmail.com>
2024-03-22 22:19:58 +0200

    created a jframe and jpanel to hold components

--- Acronym Generator/src/AbbreviationGenerator.java
+++ Acronym Generator/src/AbbreviationGenerator.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class AbbreviationGenerator {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Abbreviation Generator");
+        frame.setSize(400, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        placeComponents(panel);
+
+        // Make the frame visible
+        frame.setVisible(true);
     }
 }
